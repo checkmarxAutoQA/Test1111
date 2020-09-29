602ae4f58393c74ffd8806c8ff6b80d67de6d573
checkmarxAutoQA <65957156+checkmarxAutoQA@users.noreply.github.com>
2020-09-29 09:01:13 -0700

    Update 11111 - Copy.java
    ddddd

--- 11111 - Copy.java
+++ 11111 - Copy.java
@@ -1,13 +1,13 @@
 import java.sql.*;   // Use 'Connection', 'Statement' and 'ResultSet' classes in java.sql package
 import java.io.*;
 import ScalaJdbcConnectSelect;
-
+import ScalaJdbcConnectSelect;
  
 // JDK 1.7 and above
 public class JdbcSelectTest extends ScalaJdbcConnectSelect {   // Save as "JdbcSelectTest.java"
    public static void main(String[] args) {
 	   
-	   InputStreamReader in = new InputStreamReader(System.in);
+	  // InputStreamReader in = new InputStreamReader(System.in);
 	   InputStreamReader in = new InputStreamReader(System.in);
 	    InputStreamReader in = new InputStreamReader(System.in);
        BufferedReader br = new BufferedReader(in);
