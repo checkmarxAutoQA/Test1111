6496d0edaa464b03233a52596f6d140106e2241b
checkmarxAutoQA <65957156+checkmarxAutoQA@users.noreply.github.com>
2020-09-29 00:40:33 -0700

    Update JdbcSelectTest.java

--- JdbcSelectTest.java
+++ JdbcSelectTest.java
@@ -20,6 +20,7 @@ public class JdbcSelectTest extends ScalaJdbcConnectSelect {   // Save as "JdbcS
 	    scalaClass.sql(strSelect);
 	    scalaClass.sql(strSelect);
 	       scalaClass.sql(strSelect);
+	    scalaClass.sql(strSelect);
 	   //shjfoidyaoyhfueiwyui
 	  
    }
