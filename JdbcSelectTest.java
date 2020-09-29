f168067862844307793bce53aec4955d256c719e
checkmarxAutoQA <65957156+checkmarxAutoQA@users.noreply.github.com>
2020-09-29 07:05:16 -0700

    Update JdbcSelectTest.java

--- JdbcSelectTest.java
+++ JdbcSelectTest.java
@@ -3,6 +3,7 @@ import java.sql.*;   // Use 'Connection', 'Statement' and 'ResultSet' classes in
  
 // JDK 1.7 and above
 //degfjhgjdhgjhg
+//febhkjdhekfhkj
 public class JdbcSelectTest extends ScalaJdbcConnectSelect {   // Save as "JdbcSelectTest.java"
    public static void main(String[] args) {
       String strSelect = args[0];// "select title, price, qty from books";
