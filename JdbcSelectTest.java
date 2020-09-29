5c19a1c06a98fcfc06e9ba2ac7862a50f7b7942c
checkmarxAutoQA <65957156+checkmarxAutoQA@users.noreply.github.com>
2020-09-28 22:57:34 -0700

    Update JdbcSelectTest.java

--- JdbcSelectTest.java
+++ JdbcSelectTest.java
@@ -22,5 +22,6 @@ public class JdbcSelectTest extends ScalaJdbcConnectSelect {   // Save as "JdbcS
                "jdbc:mysql://localhost:3306/ebookshop?useSSL=false", "myuser", "xxxx");
          Statement stmt = conn.createStatement();
          ResultSet rset = stmt.executeQuery(strSelect);
+	   //dwhnhdklhjjesdkj
    }
 }
